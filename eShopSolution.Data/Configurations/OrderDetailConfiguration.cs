310c12d3ba7ce413366a5d85efe4766a5bdccece
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-07-05 15:21:34 +0700

    create_migration

---
+++ eShopSolution.Data/Configurations/OrderDetailConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.ToTable("OrderDetails");
+
+            builder.HasKey(x => new { x.OrderId, x.ProductId });
+
+            builder.HasOne(x => x.Order).WithMany(x => x.OrderDetails).HasForeignKey(x => x.OrderId); // khoá ngoại của bảng Order
+            builder.HasOne(x => x.Product).WithMany(x => x.OrderDetails).HasForeignKey(x => x.ProductId);
+        }
+    }
+}
---
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    // load file appsetting.json
+    // kết nối csdl
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())               // đường dẫn
+                .AddJsonFile("appsettings.json")                            // json file (appseting.js)
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb"); // get chuỗi kn bên appsetting.js
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
